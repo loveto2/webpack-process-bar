465b4c43c7cda41f8e2c506b02d8e2c9e648d2bb
miss-candy-girl <846471465@qq.com>
2021-06-17 00:03:26 +0800

    ProcessBar B

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -9,7 +9,7 @@ const {
   appendWebpackPlugin
 } = require('@rescripts/utilities')
 
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const webpack = require('webpack')
 
 const resolve = dir => path.join(__dirname, dir)
 
@@ -130,7 +130,7 @@ const lessConfig = config => {
 
 const processBarConfig = config => {
   return appendWebpackPlugin(
-    new ProgressBarPlugin(),
+    new webpack.ProgressPlugin(),
     config
   )
 }
